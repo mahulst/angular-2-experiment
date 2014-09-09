88707d6c06804b3a059bbe8a92886de73294b9f2
mvbschot <m.vbschot@gmail.com>
2014-09-09 21:53:18 +0200

    chore(app): apparently all routes must have a title or it will use the module name

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export class App {
             config.title = document.title;
 
             config.map([
-                { pattern: [''], componentUrl: 'routes/overview' }
+                { pattern: [''], componentUrl: 'routes/overview', title: 'Overview' }
             ]);
         });
     }
