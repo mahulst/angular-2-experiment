23aedeb19fdaf8610794f2b1754622ebd8aab618
mvbschot <m.vbschot@gmail.com>
2014-09-23 16:26:49 +0200

    fix(TitleBar): use the correct execution context when creating views

--- src/directives/title_bar.js
+++ src/directives/title_bar.js
@@ -3,46 +3,49 @@ import {Inject} from 'di';
 
 export class TitleBar {
     constructor() {
-        this.viewFactories = [];
+        this.content = [];
     }
 
-    addViewFactory(viewFactory) {
-        this.viewFactories.push(viewFactory);
+    addContent(content) {
+        this.content.push(content);
     }
 
-    removeViewFactory(viewFactory) {
-        var index = this.viewFactories.indexOf(viewFactory);
+    removeContent(content) {
+        var index = this.content.indexOf(content);
         if (index > -1) {
-            this.viewFactories.splice(index, 1);
+            this.content.splice(index, 1);
         }
     }
 }
 
 @TemplateDirective({selector: 'title-bar-content'})
-@Inject(TitleBar, BoundViewFactory)
+@Inject(TitleBar, BoundViewFactory, 'executionContext')
 @AttachAware
 export class TitleBarContent {
-    constructor(titleBar, viewFactory) {
+    constructor(titleBar, viewFactory, context) {
         this.titleBar = titleBar;
-        this.viewFactory = viewFactory;
+        this.content = {
+			viewFactory: viewFactory,
+			context: context
+		};
     }
 
     diAttached() {
-        this.titleBar.addViewFactory(this.viewFactory);
+        this.titleBar.addContent(this.content);
     }
 
     diDetached() {
-        this.titleBar.removeViewFactory(this.viewFactory);
+        this.titleBar.removeContent(this.content);
     }
 }
 
 @TemplateDirective({selector: 'title-bar-view-port'})
 @Inject(ViewPort, TitleBar)
-export class TitleBarTransclude {
+export class TitleBarViewPort {
     constructor(viewPort, titleBar) {
         this.viewPort = viewPort;
         this.titleBar = titleBar;
-        Object.observe(this.titleBar.viewFactories, () => {
+        Object.observe(this.titleBar.content, () => {
             this.viewFactoriesChanged();
         });
     }
@@ -51,9 +54,17 @@ export class TitleBarTransclude {
         if (this.attachedView) {
             this.attachedView.remove();
         }
-        this.attachedViewFactory = this.titleBar.viewFactories[this.titleBar.viewFactories.length - 1];
-        this.attachedViewFactory.viewPort = this.viewPort;
-        this.attachedView = this.attachedViewFactory.createView();
+
+        this.attachedContent = this.titleBar.content[this.titleBar.content.length - 1];
+		this.attachedContent.viewFactory.viewPort = this.viewPort;
+
+        this.attachedView = this.attachedContent.viewFactory.createView({
+			executionContext: this.attachedContent.context
+		});
+
         this.attachedView.appendTo(this.viewPort);
+
+		// TODO: The view port that shares its execution context with <title-bar-content> is not flushing
+		// at the appropriate moments (e.g., when using [on-click=""] while appended to <title-bar-view-port>)
     }
 }
