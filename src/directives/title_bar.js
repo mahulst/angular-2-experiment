f852cdb246500bb5f4beb441c43cdb26311ea08b
mvbschot <m.vbschot@gmail.com>
2014-09-25 12:49:05 +0200

    refactor(TitleBar): use annotation for the content observer and add smarter change handling

--- src/directives/title_bar.js
+++ src/directives/title_bar.js
@@ -5,11 +5,9 @@ export class TitleBar {
     constructor() {
         this.content = [];
     }
-
     addContent(content) {
         this.content.push(content);
     }
-
     removeContent(content) {
         var index = this.content.indexOf(content);
         if (index > -1) {
@@ -18,9 +16,11 @@ export class TitleBar {
     }
 }
 
-@TemplateDirective({selector: 'title-bar-content'})
-@Inject(TitleBar, BoundViewFactory, 'executionContext')
+@TemplateDirective({
+    selector: 'title-bar-content'
+})
 @AttachAware
+@Inject(TitleBar, BoundViewFactory, 'executionContext')
 export class TitleBarContent {
     constructor(titleBar, viewFactory, context) {
         this.titleBar = titleBar;
@@ -29,42 +29,47 @@ export class TitleBarContent {
             context: context
         };
     }
-
     diAttached() {
         this.titleBar.addContent(this.content);
     }
-
     diDetached() {
         this.titleBar.removeContent(this.content);
     }
 }
 
-@TemplateDirective({selector: 'title-bar-view-port'})
+@TemplateDirective({
+    selector: 'title-bar-view-port',
+    observe: {
+        'titleBar.content[]': 'contentChanged'
+    }
+})
 @Inject(ViewPort, TitleBar)
 export class TitleBarViewPort {
     constructor(viewPort, titleBar) {
         this.viewPort = viewPort;
         this.titleBar = titleBar;
-        Object.observe(this.titleBar.content, () => {
-            this.viewFactoriesChanged();
-        });
     }
 
-    viewFactoriesChanged() {
-        if (this.attachedView) {
-            this.attachedView.remove();
+    contentChanged() {
+        var oldContent;
+        if (this.content) {
+            oldContent = this.content;
         }
+        this.content = this.titleBar.content[this.titleBar.content.length - 1];
 
-        this.attachedContent = this.titleBar.content[this.titleBar.content.length - 1];
-        this.attachedContent.viewFactory.viewPort = this.viewPort;
-
-        this.attachedView = this.attachedContent.viewFactory.createView({
-            executionContext: this.attachedContent.context
-        });
-
-        this.attachedView.appendTo(this.viewPort);
-
-        // TODO: The view port that shares its execution context with <title-bar-content> is not flushing
-        // at the appropriate moments (e.g., when using [on-click=""] while appended to <title-bar-view-port>)
+        if (this.content !== oldContent) {
+            if (this.view) {
+                this.view.remove();
+            }
+            if (this.content) {
+                // TODO: The view port sharing its execution context with <title-bar-content> is not flushing
+                // when event handlers (e.g., [on-click]) are used while appended to <title-bar-view-port>
+                this.content.viewFactory.viewPort = this.viewPort;
+                this.view = this.content.viewFactory.createView({
+                    executionContext: this.content.context
+                });
+                this.view.appendTo(this.viewPort);
+            }
+        }
     }
 }
